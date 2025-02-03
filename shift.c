5727db661321a78d6dcca08b9a9fbe629e73b9f1
SANJAYMAYAKRISHNAN <sanjaymayakrishnan083@gmail.com>
2025-02-03 22:06:57 +0530

    new files

---
+++ shift.c
@@ -0,0 +1,26 @@
+/*
+int main()
+{
+    int a = 11;
+    float b = 12.3;
+    char c = 'A';
+    char name[10] = "hello";
+    double d = 12;
+
+    printf("the value is %d \n", a);
+    printf("the value is %f \n", b);
+    printf("the value is %c \n", c);
+    printf("the value is %s \n", name);
+    printf("the value is %0.2lf", d);
+    return 0;
+}
+*/
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    printf("$");
+
+    return 0;
+}
