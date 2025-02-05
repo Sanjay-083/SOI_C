39d75a16dc5d93426e0ba16924fe29a6e9b554e3
SANJAYMAYAKRISHNAN <sanjaymayakrishnan083@gmail.com>
2025-02-05 15:44:58 +0530

    v3

--- chr.c
+++ chr.c
@@ -3,9 +3,17 @@ void main()
 {
 
     // unsigned char a = -1; // unsigned chr
-    char b = 255; // signed chr
-    printf("%d", sizeof(b));
+    /* char b = 255; // signed chr
+     printf("%d", sizeof(b));
 
-    long long int a = 8438306895;
-    printf("%lld", sizeof(a));
+     long long int a = 8438306895;
+     printf("%lld", sizeof(a));*/
+
+    // while in a constant keyword use a initiqlize
+
+    // write a code snippet to print a ASIC value
+    char a;
+    printf("Enter  characters: ");
+    scanf("%c", &a);
+    printf("the ASIC id is %d", a);
 }
