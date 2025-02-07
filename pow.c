175b287e1f020ba919a2221c2380ec5a10666e87
SANJAYMAYAKRISHNAN <sanjaymayakrishnan083@gmail.com>
2025-02-07 14:28:50 +0530

    print changed

---
+++ pow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int n, pow;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Enter pow: ");
+    scanf("%d", &pow);
+
+    int res = 1;
+    while (pow > 0)
+    {
+        res = res * n;
+        pow--;
+    }
+    printf("The Answer is :%d", res);
+}
