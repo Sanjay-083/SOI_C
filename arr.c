8de3aab884db3f9dc7f8dc3c39d5373d00112cd5
SANJAYMAYAKRISHNAN <sanjaymayakrishnan083@gmail.com>
2025-02-10 16:00:01 +0530

    sum

--- arr.c
+++ arr.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
-void main()
+
+int main()
 {
-
     int size;
-    printf("Enter the size::");
+    printf("Enter the size: ");
     scanf("%d", &size);
-
     int arr[size];
-    for (int i = 0; i <= size; i++)
+    // Input elements for the array
+    for (int i = 0; i < size; i++)
     {
-        printf("enter the arr %d::\n", i);
+        printf("Enter the element arr %d: ", i);
         scanf("%d", &arr[i]);
     }
-
-    for (int i = 0; i <= size; i++)
+    // Display elements of the array
+    printf("The elements in the array are:\n");
+    for (int i = 0; i < size; i++)
     {
-        printf("enter the arr ::%d\n", arr[i]);
+        printf("arr[%d] = %d\n", i, arr[i]);
     }
+
+    // Calculate the sum of the elements in the array
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    printf("The sum of the elements in the array is: %d\n", sum);
+
+    return 0;
 }
